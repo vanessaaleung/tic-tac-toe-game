16cdfe9254410c36281a91c1ef21c7c033057a23
Vanessa Leung <leungkachiiiii@gmail.com>
2020-03-24 12:55:05 -0700

    Updates

---
+++ precache-manifest.51510ea7ad2d6b747fd64422a658c85e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c746aaad65db75074993224b6300bc11",
+    "url": "/tic-tac-toe-game/index.html"
+  },
+  {
+    "revision": "fac0e28dc71ec16140b3",
+    "url": "/tic-tac-toe-game/static/css/main.9f95df49.chunk.css"
+  },
+  {
+    "revision": "f6f7ab4316fa5206efac",
+    "url": "/tic-tac-toe-game/static/js/2.d92886f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-game/static/js/2.d92886f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac0e28dc71ec16140b3",
+    "url": "/tic-tac-toe-game/static/js/main.da887d3c.chunk.js"
+  },
+  {
+    "revision": "103c4b731c7454e78af3",
+    "url": "/tic-tac-toe-game/static/js/runtime-main.f6d63920.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tic-tac-toe-game/precache-manifest.666aafa27af3fc942b9ea45e8407a5ac.js"
+  "/tic-tac-toe-game/precache-manifest.51510ea7ad2d6b747fd64422a658c85e.js"
 );
 
 self.addEventListener('message', (event) => {
